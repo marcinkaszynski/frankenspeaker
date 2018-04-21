1e609bde231387b60830c70904ad350c369fa97e
Marcin Kaszynski <marcin.kaszynski@gmail.com>
2018-04-21 20:35:17 +0000

    Control volume

--- frankenspeaker.py
+++ frankenspeaker.py
@@ -1,11 +1,16 @@
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
+import RPi.GPIO as GPIO
 import alsaaudio
 import time
 
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+
+
+MAX_VOL = 100
+MIN_VOL = 0
 
 
 class Vis(object):
@@ -17,11 +22,9 @@ class Vis(object):
         self.font = ImageFont.load_default()
 
         self.volume = None
-        self.max_volume = 100
         self.dirty = False
 
     def set_volume(self, volume):
-        print("SV", self.volume, volume)
         if self.volume != volume:
             self.dirty = True
             self.volume = volume
@@ -32,23 +35,59 @@ class Vis(object):
 
         draw = ImageDraw.Draw(self.image)
         draw.rectangle((0, 0, self.disp.width-1, self.disp.height-1), outline=0, fill=0)
-        draw.text((0, 0), '%d / %d' % (self.volume, self.max_volume), font=self.font, fill=255)
+        draw.text((0, 0), '%d / %d' % (self.volume, MAX_VOL), font=self.font, fill=255)
         self.disp.image(self.image)
         self.disp.display()
         self.dirty = False
 
 
+class Buttons(object):
+    def __init__(self, vol_dn, vol_up):
+        self.last_ts = time.time()
+        self.vol_dn = vol_dn
+        self.vol_up = vol_up
+        GPIO.setup(self.vol_dn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.vol_up, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        self.down = self.up = False
+
+    def update(self, ts):
+        self.down = GPIO.input(self.vol_dn)
+        self.up = GPIO.input(self.vol_up)
+
+
+def update_volume(buttons, vol):
+    new_vol = vol
+
+    if buttons.up:
+        new_vol += 5
+    if buttons.down:
+        new_vol -= 5
+
+    new_vol = min(max(new_vol, MIN_VOL), MAX_VOL)
+    return (new_vol, vol != new_vol)
+
+
 def main_loop():
     vis = Vis()
+    buttons = Buttons(20, 21)
     while True:
+        ts = time.time()
+
         # Don't care about stereo, we'll even levels on first volume
         # change anyway.
         mixer = alsaaudio.Mixer('Speaker')
-        vis.set_volume(max(mixer.getvolume()))
+        vol = max(mixer.getvolume())
 
-        ts = time.time()
+        buttons.update(ts)
+        (vol, changed) = update_volume(buttons, vol)
+        if changed:
+            mixer.setvolume(vol, alsaaudio.MIXER_CHANNEL_ALL)
+
+        vis.set_volume(vol)
         vis.update_display(ts)
         time.sleep(0.1)
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
     main_loop()
